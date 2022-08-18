4d2b049bf9ba47a203401029aec2f7cb9e526059
fixedOtter <fo1152rc@go.minnstate.edu>
2022-08-18 10:08:02 -0500

    fixed my weird query problem; added more routes for CRUD

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,16 @@ apiRouter.get('/users', async(req, res) => {
   }
 });
 
+// get one soshmeed user
+apiRouter.get('/user', async(req, res) => {
+  try {
+    const foundUser = await User.findOne({ _id: ObjectId(req.body.userID) }).populate('thoughts').populate('friends');
+    res.json(foundUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // create a new soshmeed user
 apiRouter.post('/user', async(req, res) => {
   try {
@@ -45,18 +55,20 @@ apiRouter.put('/user', async(req, res) => {
 apiRouter.post('/thoughts', async(req, res) => {
   try {
     const user = await User.findOne({ _id: ObjectId(req.body.userID) }); // looking for user by req id
-    // user.thoughts.push({ title: req.body.thoughtTitle, body: req.body.thoughtBody }); // pushing new thought to the user's thought array (following the thought schema)
-    // user.save(); // saving changes to user
+    const newThought = await Thought.create({title: req.body.title, body: req.body.body })
+    user.thoughts.push(newThought._id); // pushing new thought to the user's thought array (following the thought schema)
+    user.save(); // saving changes to user
     res.json(user); // responding with newly updated user
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-// update 
+// update thought on soshmeed user
 apiRouter.put('/thought', async(req, res) => {
   try {
-    
+    const user = await User.findOne({ _id: ObjectId(req.body.userID) });
+    const updatedThought = await Thought.findByIdAndUpdate({ _id: req.body.thoughtID})
   } catch (error) {
     res.status(500).json(error);
   }
