db268a85d3c4f77696f4d3f577c57ccd830093d0
fixedOtter <fo1152rc@go.minnstate.edu>
2022-08-18 21:46:31 -0500

    flushed out the CRUD routes boii

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@
 
 const apiRouter = require('express').Router();
 const { ObjectId } = require('mongodb');
-const { User, Thought } = require('../models');
+const { User, Thought, Reaction } = require('../models');
 
 // get all soshmeed users
 apiRouter.get('/users', async(req, res) => {
@@ -36,14 +36,15 @@ apiRouter.post('/user', async(req, res) => {
 });
 
 // update user stuffs
-apiRouter.put('/user', async(req, res) => {
+apiRouter.put('/user', async(req, res) => { 
   try {
     const user = await User.findOneAndUpdate( // finding one user and updating
       {_id: ObjectId(req.body.userID)}, // looking for user by id
       {
         username: req.body.username, // passing in username and pass from req.body
         password: req.body.password
-      }
+      },
+      { new: true } // specifies for us to grab the updated user - not the pre-update
     );
     res.json(user); // sending back new user
   } catch (error) {
@@ -51,8 +52,20 @@ apiRouter.put('/user', async(req, res) => {
   }
 });
 
+// delete user
+apiRouter.delete('/user', async(req, res) => {
+  try {
+    const delUser = await User.findOneAndDelete({ _id: ObjectId(req.body.userID) }); // deleting user from req body
+    const thoughts = await Thought.deleteMany({ _id: { $in: delUser.thoughts }}); // deleting the thoughts from the user
+    await Reaction.deleteMany({ _id: { $in: thoughts.reactions }}); // deleting the reactions from all the thoughts
+    res.send(delUser); // sending back the del data
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // create thought on soshmeed user
-apiRouter.post('/thoughts', async(req, res) => {
+apiRouter.post('/thought', async(req, res) => {
   try {
     const user = await User.findOne({ _id: ObjectId(req.body.userID) }); // looking for user by req id
     const newThought = await Thought.create({title: req.body.title, body: req.body.body })
@@ -64,14 +77,68 @@ apiRouter.post('/thoughts', async(req, res) => {
   }
 });
 
-// update thought on soshmeed user
-apiRouter.put('/thought', async(req, res) => {
+// read thought by id
+apiRouter.get('/thought', async(req, res) => {
   try {
-    const user = await User.findOne({ _id: ObjectId(req.body.userID) });
-    const updatedThought = await Thought.findByIdAndUpdate({ _id: req.body.thoughtID})
+    const thought = await Thought.findOne({ _id: ObjectId(req.body.thoughtID) }).populate('reactions');
+    res.send(thought);
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
+// update thought on soshmeed user
+apiRouter.put('/thought', async(req, res) => {
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate( // finding thought by id
+      { _id: req.body.thoughtID}, // filter condition is thought id
+      {
+        title: req.body.title, // updating title && body from req.body
+        body: req.body.body
+      },
+      { new: true } // sending back the updated thought obj
+    );
+    res.json(updatedThought);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// delete thought by id
+apiRouter.delete('/thought', async(req, res) => {
+  try {
+    const delThought = await Thought.findOneAndDelete({ _id: ObjectId(req.body.thoughtID) }); // deleting thought from req body
+    await React.deleteMany({ _id: { $in: delThought.reactions }}); // deleting the reactions from the thought
+    res.send(delThought); // sending back the deleted thought
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// add friend to user
+apiRouter.put('/friend', async(req, res) => {
+  try {;
+    const user = await User.findOne({_id: ObjectId(req.body.userID)});
+    user.friends.push(req.body.friendID);
+    await user.save();
+    res.json(user); // sending back updated user
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// remove friend from user
+apiRouter.delete('/friend', async(req, res) => {
+  try {
+    const user = await User.findOne({_id: ObjectId(req.body.userID)});
+    user.friends.remove(req.body.friendID);
+    await user.save();
+    res.json(user); // sending back updated user
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
 
 module.exports = apiRouter;
